cdc12119b09845354e268a17038ca1071e65236d
Jaume Cortés <79199660+cortes0702@users.noreply.github.com>
2022-02-24 13:58:42 +0100

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,61 +1,96 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const async = require("hbs/lib/async")
+const { find } = require("../models/db")
+const router = express.Router();
+
+//DB
+const db = require("../models/db")
+const cars = require("../models/cars")
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+  res.render('index');
 });
 
+router.get("/api", async (req, res) => {
+  const coches = await cars.find({})
+  res.json(coches)
+})
+
 //Add cars 
-router.get('/add-cars', function(req, res) {
+router.get('/add-cars', (req, res) => {
   res.render('add-cars')
 })
 
-router.post('/add-cars', function(req, res, next) {
-  res.send("ok")
-  //var data = req.body
-  //console.log(data)
-  //await data.save()
-  //res.send(data.json())
+router.post('/add-cars-form', async (req, res, next) => {
+  const data = new cars(req.body)
+  await data.save()
+  res.redirect("/add-cars")
+})
+
+router.get("/add-cars-form", async (req, res, next)  => {
+  const data = await cars.find({})
+  res.json(data)
 })
 
 //List Cars
-router.get("/list-cars", function(req, res) {
-  res.render("list-cars")
+router.get("/list-cars", async (req, res, next) => {
+  const coches = await cars.find({})
+  res.render('list-cars', {coches})
 })
 
-router.post('/list-cars', function(req, res, next) {
-  res.send("ok")
-  //var data = req.body
-  //console.log(data)
-  //await data.save()
-  //res.send(data.json())
+//Update y eliminar cars
+router.get("/update-car/:id", async (req, res, next) => {
+  const coche_encontrado = await cars.findById(req.params.id)
+  res.render("update-car", {coche_encontrado})
+})
+
+router.put('/updated-car/:id', async (req, res, next) => {
+  await cars.findByIdAndUpdate(req.params.id, req.body)
+  res.redirect('/list-cars')
+})
+
+router.delete('/delete-car/:id', async (req, res) => {
+  await cars.findByIdAndDelete(req.params.id)
+  res.redirect('/list-cars')
 })
 
 //Add Users
-router.get("/add-users", function(req, res) {
+router.get("/add-users", (req, res) => {
   res.render("add-users")
 })
 
-router.post('/add-users', function(req, res, next) {
-  res.send("ok")
-  //var data = req.body
-  //console.log(data)
-  //await data.save()
-  //res.send(data.json())
+router.post('/add-users-form', async (req, res, next) => {
+  const data = new db(req.body)
+  await data.save()
+  res.redirect("/add-users")
+})
+
+router.get("/add-users-form", async (req, res, next)  => {
+  const data = await db.find({})
+  res.json(data)
 })
 
 //List Users
-router.get("/list-users", function(req, res) {
-  res.render('list-users')
+router.get("/list-users", async (req, res, next) => {
+  const usuarios = await db.find({})
+  res.render('list-users', {usuarios})
 })
 
-router.post('/list-users', function(req, res, next) {
-  res.send("ok")
-  //var data = req.body
-  //console.log(data)
-  //await data.save()
-  //res.send(data.json())
+//Update y eliminar users
+router.get("/update-user/:id", async (req, res, next) => {
+  const usuario_encontrado = await db.findById(req.params.id)
+  res.render("update-user", {usuario_encontrado})
+})
+
+router.put('/updated-user/:id', async (req, res, next) => {
+  await db.findByIdAndUpdate(req.params.id, req.body)
+  res.redirect('/list-users')
+})
+
+router.delete('/delete-user/:id', async (req, res) => {
+  await db.findByIdAndDelete(req.params.id)
+  res.redirect('/list-users')
 })
 
 module.exports = router;
